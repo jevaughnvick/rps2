f34afdb0e4a47e7a97f8fe58da666add5efed723
Jevaughn Vickers <114950498+jevaughnvick@users.noreply.github.com>
2024-07-03 19:46:20 -0400

    write functions to connect DOM with game logic and state

--- main.js
+++ main.js
@@ -1,4 +1,33 @@
+const buttons = document.querySelectorAll("button");
 
+const mainScreen = document.querySelector(".main-screen");
+const playerOneResult = document.querySelector(".child-one");
+const computerResult = document.querySelector(".child-two");
+
+
+let humanScore = 0;
+let computerScore = 0;
+
+
+buttons.forEach(button => button.addEventListener("click", e => {
+
+    const humanChoice = recordPlayerSelection(e);
+    const computerChoice = getComputerChoice();
+    
+    playRound(humanChoice, computerChoice);
+
+    if(!(humanScore < 5 && computerScore < 5)){
+
+        let winner = getWinner(humanScore, computerScore);
+
+        showWinner(winner);
+        humanScore = 0;
+        playerOneResult.textContent = `Player One: ${humanScore}`;;
+        computerScore = 0;
+        computerResult.textContent = `Computer: ${humanScore}`;;
+    };
+
+}));
 
 function getComputerChoice(){
     switch(Math.floor(Math.random() * 3)){
@@ -11,6 +40,11 @@ function getComputerChoice(){
         case 2:
             return "Scissors"
     }
+}
+
+function recordPlayerSelection(e){
+
+    return e.target.textContent;
 }
 
 function getHumanChoice(){
@@ -28,73 +62,57 @@ function getHumanChoice(){
     }
 }
 
-
-function playGame(){
-
-    let humanScore = 0;
-    let computerScore = 0;
-
+function playRound(humanChoice, computerChoice){
     
+    let roundWinner = getRoundWinner(humanChoice, computerChoice);
+    if (roundWinner === "Human") humanScore++
+    if (roundWinner === "Computer") computerScore++
 
-    while(humanScore < 5 && computerScore < 5){
+    showRoundWinner(roundWinner, humanChoice, computerChoice);
 
-        let humanChoice = getHumanChoice();
-        let computerChoice = getComputerChoice();
-        playRound(humanChoice, computerChoice);
-        
-    }
-
-    let winner = getWinner(humanScore, computerScore);
-
-    showWinner(winner);
-
-    ///////////////////////////////////////////////////////////////////////
-
-    function playRound(humanChoice, computerChoice){
-    
-        let roundWinner = getRoundWinner(humanChoice, computerChoice);
-        if (roundWinner === "Human") humanScore++
-        if (roundWinner === "Computer") computerScore++
-    
-        showRoundWinner(roundWinner, humanChoice, computerChoice);
-    }
-    
-    function getRoundWinner(humanChoice, computerChoice){
-    
-        if (humanChoice === "Rock" && computerChoice === "Rock") return "Draw"
-        if (humanChoice === "Rock" && computerChoice === "Paper") return "Computer"
-        if (humanChoice === "Rock" && computerChoice === "Scissors") return "Human"
-        if (humanChoice === "Paper" && computerChoice === "Rock") return "Human"
-        if (humanChoice === "Paper" && computerChoice === "Paper") return "Draw"
-        if (humanChoice === "Paper" && computerChoice === "Scissors") return "Computer"
-        if (humanChoice === "Scissors" && computerChoice === "Rock") return "Computer"
-        if (humanChoice === "Scissors" && computerChoice === "Paper") return "Human"
-        if (humanChoice === "Scissors" && computerChoice === "Scissors") return "Draw"
-    }
-    
-    function showRoundWinner(roundWinner, humanChoice, computerChoice){
-
-        if(roundWinner === "Draw") console.log("That's a draw!")
-        if(roundWinner === "Human") console.log(`You Win! ${humanChoice} beats ${computerChoice}`)
-        if(roundWinner === "Computer") console.log(`You Lose! ${computerChoice} beats ${humanChoice}`)
-    }
-
-    function getWinner(humanScore, computerScore){
-
-        if(humanScore > computerScore){
-            return "human"
-        }else{
-            return "computer"
-        }
-    
-    }
-    
-    function showWinner(winner){
-
-        if(winner === "human") console.log("You Won the Game!");
-        if(winner === "computer") console.log("You Lost the Game!");
-    }
 }
 
+function getRoundWinner(humanChoice, computerChoice){
+    
+    if (humanChoice === "Rock" && computerChoice === "Rock") return "Draw"
+    if (humanChoice === "Rock" && computerChoice === "Paper") return "Computer"
+    if (humanChoice === "Rock" && computerChoice === "Scissors") return "Human"
+    if (humanChoice === "Paper" && computerChoice === "Rock") return "Human"
+    if (humanChoice === "Paper" && computerChoice === "Paper") return "Draw"
+    if (humanChoice === "Paper" && computerChoice === "Scissors") return "Computer"
+    if (humanChoice === "Scissors" && computerChoice === "Rock") return "Computer"
+    if (humanChoice === "Scissors" && computerChoice === "Paper") return "Human"
+    if (humanChoice === "Scissors" && computerChoice === "Scissors") return "Draw"
+}
 
-playGame()
+function showRoundWinner(roundWinner, humanChoice, computerChoice){
+
+    if(roundWinner === "Draw") mainScreen.textContent = "That's a draw!";
+    if(roundWinner === "Human"){
+
+        mainScreen.textContent = `You Win! ${humanChoice} beats ${computerChoice}`;
+        playerOneResult.textContent = `Player One: ${humanScore}`;
+    };
+    if(roundWinner === "Computer"){
+
+        mainScreen.textContent = `You Lose! ${computerChoice} beats ${humanChoice}`;
+        computerResult.textContent = `Computer: ${computerScore}`;
+    };
+}
+
+function getWinner(humanScore, computerScore){
+
+    if(humanScore > computerScore){
+        return "human"
+    }else{
+        return "computer"
+    }
+
+}
+
+function showWinner(winner){
+
+    if(winner === "human") mainScreen.textContent = "You Won the Game!";
+    if(winner === "computer") mainScreen.textContent = "You Lost the Game!";
+}
+
